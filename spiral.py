85e9d1dc08e4b8f169c77fa66dd4420c6a3d6d10
t0m0h1 <104913403+t0m0h1@users.noreply.github.com>
2024-06-09 19:05:07 +0100

    Refactor code to improve performance and readability

---
+++ spiral.py
@@ -0,0 +1,25 @@
+import turtle
+
+def draw_spiral():
+    # window = wn
+    wn = turtle.Screen()
+    wn.bgcolor("lightblue")
+
+    # spiral = turtle
+    spiral = turtle.Turtle()
+    spiral.pensize(2)
+    spiral.speed(0)
+
+    colours = ["red", "purple", "blue", "green", "yellow", "orange"]
+
+    for i in range(360):
+        spiral.pencolor(colours[i % 6])
+        spiral.width(i / 100 + 1)
+        spiral.forward(i)
+        spiral.left(59)
+
+    spiral.hideturtle()
+    wn.exitonclick()
+
+if __name__ == "__main__":
+    draw_spiral()
